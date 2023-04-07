ebcb3a6ea081bbd2ca79c5e3fa85e0d4bf6fa43d
Arjun Verma <averma332@gatech.edu>
2023-04-06 22:11:37 -0400

    new

---
+++ ClientBackup/main.c
@@ -0,0 +1,98 @@
+#include <Arduino.h>
+#include <WiFiNINA.h>
+#include <WiFiUdp.h>
+#include <wifistuff.h>
+
+/** Audio Sampling **/
+#define SOUND_BUFFER_SIZE 400
+#define NUM_BUFFERS 2
+volatile int buffer_counter;
+int16_t sound_buffer[NUM_BUFFERS][SOUND_BUFFER_SIZE];
+int active_buffer;
+/** **/
+
+/** UDP Client **/
+int status = WL_IDLE_STATUS;
+char ssid[] = MYSSID;    // network SSID (name)
+char pass[] = MYPASS;    // network password (use for WPA, or use as key for WEP)
+unsigned int localPort = remotePORT - 1;
+uint16_t remotePort = remotePORT;
+WiFiUDP Udp;
+/** **/
+
+ISR(TCB0_INT_vect) {
+  if (buffer_counter < SOUND_BUFFER_SIZE && !(ADC0.COMMAND & ADC_STCONV_bm)) {
+    sound_buffer[active_buffer][buffer_counter] = ADC0.RES;
+    ADC0.COMMAND = ADC_STCONV_bm;
+    buffer_counter++;
+  }
+  TCB0.INTFLAGS = TCB_CAPT_bm;
+}
+
+void WifiSetup() {
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+    // wait 10 seconds for connection:
+    delay(5000);
+  }
+  Serial.println("Connected to WiFi");
+  Serial.println("\nStarting connection to server...");
+  Udp.begin(localPort);
+  Serial.println("Connected!");
+}
+
+void setup() {
+  Serial.begin(9600);
+  buffer_counter = 0;
+  active_buffer = 0;
+
+  /** Interrupt Handler setup **/
+  cli();
+  TCB0.CTRLB = TCB_CNTMODE_INT_gc; // Use timer compare mode
+  TCB0.CCMP = 31; // Value to compare with - 250 khz divided by this value, 31 for ~8000 Hz
+  TCB0.INTCTRL = TCB_CAPT_bm; // Enable the interrupt
+  TCB0.CTRLA = TCB_CLKSEL_CLKTCA_gc | TCB_ENABLE_bm; // Use Timer A as clock, enable timer
+  sei();
+  /** **/
+
+  /** Async analog read setup **/
+  int pin = digitalPinToAnalogInput(A0);
+  ADC0.MUXPOS = (pin << ADC_MUXPOS_gp);
+  ADC0.COMMAND = ADC_STCONV_bm;
+  /** **/
+
+  /** Wifi Setup **/
+  WifiSetup();
+  /** **/
+}
+
+void loop() {
+  if (buffer_counter == SOUND_BUFFER_SIZE) {
+    /** Reset relevant variables without interruptions */
+    noInterrupts();
+    buffer_counter = 0;
+
+    void* tempBuffer = (void*)(sound_buffer[active_buffer]);
+    char* packetBuffer = (char*)tempBuffer;
+
+    active_buffer = (active_buffer == 0) ? 1 : 0;
+    interrupts();
+    /** **/
+
+    /** Begin data transfer on now inactive buffer **/
+    Udp.beginPacket(remoteIP, remotePort);
+    Udp.write(packetBuffer, SOUND_BUFFER_SIZE * sizeof(int16_t));
+    Udp.endPacket();
+    /** **/
+  }
+}
---
+++ server.py
@@ -0,0 +1,29 @@
+import socket
+import struct
+import time
+import wifiutils
+
+import matplotlib.pyplot as plt
+from scipy.fft import fft, fftfreq
+import numpy as np
+
+localIP     = wifiutils.ip
+localPort   = wifiutils.port
+
+bufferSize  = 400
+unpackString = '<' + ''.join(['h' for a in range(bufferSize)])
+
+# Create a datagram socket
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Bind to address and ip
+UDPServerSocket.bind((localIP, localPort))
+print("UDP server up and listening on " + localIP + " at point " + str(localPort))
+
+# Listen for incoming datagrams
+while(True):
+    data, addr = UDPServerSocket.recvfrom(bufferSize * 2 + 8)
+
+    if (len(data) == bufferSize * 2):
+        soundData = struct.unpack(unpackString, data)
+        print(soundData)
